fed2a1ff06ac137dcef1d4d94e02df44a4a46d32
Robert Gingras <developer@three-point-five.dev>
2023-12-17 15:15:01 -0500

    updated some of the encoding cable code

--- app/javascript/channels/encoding_progress_channel.js
+++ app/javascript/channels/encoding_progress_channel.js
@@ -1,28 +1,40 @@
 import consumer from "channels/consumer"
 
-const attachements = document.querySelectorAll('[data-blob-id]')
-for (const attachment of attachements) {
-  const blob = attachment.dataset.blobId
-  consumer.subscriptions.create({
-    channel: "EncodingProgressChannel",
-    blob_id: blob
-  }, {
-    connected() {
-      // Called when the subscription is ready for use on the server
-      const display = document.createElement('div')
-      display.setAttribute('id', `blob-${blob}`)
-      display.textContent = "0"
-      attachment.appendChild(display)
-    },
+const subscriptions = []
+document.addEventListener('turbo:load', () => {
+  const attachements = Array.from(document.querySelectorAll('[data-blob-id]'))
+  const blobs = attachements.map((attachement) => attachement.dataset.blobId)
+  for (const blob of blobs) {
+    if (subscriptions[blob]) return
+    
+    subscriptions[blob] = consumer.subscriptions.create({
+      channel: "EncodingProgressChannel",
+      blob_id: blob
+    }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+        const attachment = document.querySelector(`[data-blob-id="${blob}"]`)
+        const display = document.createElement('div')
+        display.setAttribute('id', `blob-${blob}`)
+        display.textContent = "0"
+        attachment.appendChild(display)
+      },
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      const display = document.getElementById(`blob-${blob}`)
-      display.textContent = data
-    }
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        const display = document.getElementById(`blob-${blob}`)
+        display.textContent = data
+      }
+    })
+  }
+  subscriptions.forEach((sub, i) => {
+    if (blobs.includes(`${i}`)) return
+
+    sub.unsubscribe()
+    delete subscriptions[i]
   })
-}
+})
--- app/jobs/encode_upload_job.rb
+++ app/jobs/encode_upload_job.rb
@@ -4,13 +4,11 @@ class EncodeUploadJob < ApplicationJob
   queue_as :default
 
   def perform(blob_id)
-    logger.debug "Performing Encoding job for #{blob_id}"
     blob = ActiveStorage::Blob.find(blob_id)
     blob.open do |file|
       fd = Encoder.new file.path
 
       # apply 2 pass h264 encoding and convert audio to aac
-      logger.debug "Started first pass of #{file.path}"
       fd.run(:first_pass) do |progress|
         EncodingProgressChannel.broadcast_to(blob, progress)
       end
